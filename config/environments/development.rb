fd67b1149eb2f5e53eafc12467e9932f27e2c7de
João Otávio <joaotavioos@outlook.com>
2021-12-13 00:31:26 -0300

    Fix reset email template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
 
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
